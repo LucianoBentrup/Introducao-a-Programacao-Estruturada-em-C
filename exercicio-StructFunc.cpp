a26376594975fcd169b30e1e8c90178e67520829
LucianoBentrup <lucianobentrup@gmail.com>
2021-09-10 11:52:08 -0300

    Update exercicio-StructFunc.cpp

--- exercicio-StructFunc.cpp
+++ exercicio-StructFunc.cpp
@@ -7,31 +7,64 @@ using namespace std;
 //Exibir cadastros 
 //Menu com opção de 1 - Cadastrar 2 - Exibir 9 - Sair 
 
-int opcao; //Para Escolher a opcao no menu.
+    int opcao; //Para Escolher a opcao no menu.
 
-    struct pessoa
-    {
-    string nome;
-    int idade;
-    };//fim estrutra "pessoa".
+        struct pessoa
+        {
+        string nome;
+        int idade;
+        };//fim estrutra "pessoa".
 
-    pessoa p; // Estrutura "pessoa" = p 
+        pessoa p; // Estrutura "pessoa" = p 
  
-    void cadastrar()    
-    {
-        cout << "Insira seu nome:";
-        cin >> p.nome; 
-        cout << "Insira sua idade:";
-        cin >> p.idade;
+        void cadastrar()   
+        {
+            cout << "Insira seu nome:";
+            cin >> p.nome; 
+            cout << "Insira sua idade:";
+            cin >> p.idade;
 
-            bool int_idade, string_nome; //True or false
-            int_idade=((int)p.idade == p.idade);
-            string_nome=((string)p.nome == p.nome);
+                bool int_idade, string_nome; //True or false
+                int_idade=((int)p.idade == p.idade);
+                string_nome=((string)p.nome == p.nome);
 
-            if (int_idade&&string_nome)
+                if (int_idade&&string_nome)
                 cout << "Cadastro efetuado com sucesso!";
-            else 
+                else 
                 cout << "Falha ao cadastrar!";
 
-    }//Fim Funcao "cadastrar"
+        }//Fim Funcao "cadastrar"
+
+    
+        void exibir()
+        {
+        cout << "Nome:" << p.nome; 
+        cout << "Idade:" << p.idade;
+        }//Fim funcao "exibir"
+
+    int main()
+    {
+        // Inicio Menu
+        cout << "===============Menu===============" << endl;
+        cout << "Opcoes:" << endl;
+        cout << "1 - Cadastrar" <<endl;
+        cout << "2 - Exibir" << endl; 
+        cout << "9 - Sair" << endl;
+        cout << "Qual opcao voce deseja executar" << endl;
+        cin >> opcao; 
+        // Fim Menu
+
+        // Switch :)
+        switch (opcao)
+        {
+            case 1: cadastrar(); break;
+            case 2: exibir(); break;
+            case 9: cout << "Obrigado por usar meu programa!"; break;
+            default: cout << "Opcao Invalida! Tente novamente com uma das opcoes acima."; break;
+            
+        }// Fim Switch 
+        
+        
+
+    } // Fim main
 
