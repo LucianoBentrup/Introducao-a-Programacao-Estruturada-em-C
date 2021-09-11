286b10b429fcf94224e68cf4bc7e2b0b12861a8d
LucianoBentrup <70353582+LucianoBentrup@users.noreply.github.com>
2021-09-11 12:28:03 -0300

    Update Exercicio-func.cpp

--- Exercicio-func.cpp
+++ Exercicio-func.cpp
@@ -15,17 +15,17 @@ le 2 valores escolhe a operacao e exibe o resultado
 */
 
 float soma(float a, float b) {
-  return (a + b);
+  return (a + b); //funcao soma
 }
 
 float subtracao(float a, float b) {
-  return (a - b);
+  return (a - b); //funcao sub
 }
 float multiplicacao(float a, float b) {
-  return (a * b);
+  return (a * b); //funcao multiplicacao
 }
 float divisao(float a, float b) {
-  return (a / b);
+  return (a / b); //funcao divisao 
 }
 
 
@@ -37,21 +37,24 @@ int main() {
 
   while (opcao != 9){
 
-    cout<<"\n*********************************\n";
-    cout << "\n\t Calculadora Cientifica\n\n";
-    cout<<"\n*********************************\n";
-    cout<<"\nDigite valor 1\n";
-    cin>>v1;
-    cout<<"\nDigite valor 2\n";
-    cin>>v2;
-
+     // Menu
     cout<<"\n 1 - somar \n";
     cout<<"\n 2 - subtrair \n";
     cout<<"\n 3 - multiplicar \n";
     cout<<"\n 4 - dividir \n";
     cout<<"\n 9 - sair \n";
     cout<<"\n Escolha a operacao \n";
-    cin>>opcao;
+    cin>>opcao; 
+    //opções para o menu 
+
+
+    cout << "Insira os valores:" << endl; 
+    cout<<"\nDigite valor 1:\n";
+    cin>>v1;
+    cout<<"\nDigite valor 2:\n";
+    cin>>v2;
+
+   
 
     if (opcao ==1 ) {
         cout << v1 << "+" << v2 << "=" << soma(v1,v2)<<endl;
