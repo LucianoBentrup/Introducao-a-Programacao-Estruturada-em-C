0924b52c3797794b628c5be8b01bf9296797b18d
LucianoBentrup <lucianobentrup@gmail.com>
2021-09-14 12:03:18 -0300

    Create Exercicio2-func.cpp

---
+++ Exercicio2-func.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std; 
+
+/**
+6 – EXERCICIO
+
+Faça um programa para cadastrar no máximo 5 nomes. Deverá também exibir todos os nomes cadstrados. Utilize  conceito de menu.
+
+*/
+
+#define MAX 5 
+
+struct pessoa 
+{
+    string nome; 
+
+};//Fim pessoa 
