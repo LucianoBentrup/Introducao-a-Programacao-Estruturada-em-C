2bf35ca805e25b4c736569219d55b4ae209f119c
LucianoBentrup <lucianobentrup@gmail.com>
2021-09-16 19:38:36 -0300

    Create Exercicio-aula4.cpp

---
+++ Exercicio-aula4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std; 
+
+
+void salario()
+{
+    int valor=0; 
+
+    cout << "Digite o salario:" << endl; 
+    cin >> valor; 
+
+    valor = valor + 50;
+
+
+}
