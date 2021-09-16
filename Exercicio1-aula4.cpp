b7390644148639999c20e2815ccbeba55f2a7e98
LucianoBentrup <lucianobentrup@gmail.com>
2021-09-16 19:48:11 -0300

    exerc

---
+++ Exercicio1-aula4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std; 
+
+
+int exerc1(int numero)
+{
+    numero = numero + 50;
+
+    return numero;
+}
+
+int main() 
+{
+    int numero;
+    cout << "Insira o valor: ";
+    cin >> numero;
+
+
+}
