ac34300265e8ddf1c7bf101ad4ce3c434a89a4c8
LucianoBentrup <lucianobentrup@gmail.com>
2021-09-16 21:50:24 -0300

    Update vetor1-aula4.cpp

--- vetor1-aula4.cpp
+++ vetor1-aula4.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
 using namespace std; 
 
-int vetor[]={0,50,20,78,100,89,15,16,46,63,82,78};
+
 
 #define MAX 12
-void exibir(int v[]) {
+void exibir(int v[]) 
+{
 
-    for (int x=0; x<MAX;x++)
+    for (int x=0; x<=MAX;x++)
     {
         if (v[x] > 30) 
-            cout << "Posição: " 
+            cout << "Indice [" <<x<< "]" << v[x] <<endl;
     }
+}
+
+int main()
+{
+
+    
+    int vetor[]={0,50,20,78,100,89,15,16,46,63,82,78};
+    int vetor1[]={0,50,20,78,100,89,15,16,46,63,82,80};
+
+    cout << "Os valores sao: " << endl;
+    exibir(vetor);
 }
 
 
