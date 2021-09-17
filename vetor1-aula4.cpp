4196c8f8a8e51b860be6fe24139298100e5735e5
LucianoBentrup <lucianobentrup@gmail.com>
2021-09-17 15:06:51 -0300

    Update vetor1-aula4.cpp

--- vetor1-aula4.cpp
+++ vetor1-aula4.cpp
@@ -6,12 +6,15 @@ using namespace std;
 #define MAX 12
 void exibir(int v[]) 
 {
-
-    for (int x=0; x<=MAX;x++)
+    int soma;
+    for (int x=0; x<MAX;x++)
     {
-        if (v[x] > 30) 
-            cout << "Indice [" <<x<< "]" << v[x] <<endl;
+       soma = soma + v[x];
+       cout << "Indices [" << x << "] :" << v[x] << endl; 
     }
+
+cout << "Soma = " << soma << endl;
+
 }
 
 int main()
@@ -21,7 +24,7 @@ int main()
     int vetor[]={0,50,20,78,100,89,15,16,46,63,82,78};
     int vetor1[]={0,50,20,78,100,89,15,16,46,63,82,80};
 
-    cout << "Os valores sao: " << endl;
+    
     exibir(vetor);
 }
 
